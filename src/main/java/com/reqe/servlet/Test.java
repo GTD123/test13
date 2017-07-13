68167e4cc41d15ec18414fbcdc38a8f7940dd116
RQ <791538998@qq.com>
2017-07-13 17:57:13 +0800

    提交代码

---
+++ src/main/java/com/reqe/servlet/Test.java
@@ -0,0 +1,14 @@
+package com.reqe.servlet;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by RQ on 2017/7/11.
+ */
+public class Test {
+    private static Logger log = LoggerFactory.getLogger(Test.class);
+    public static void main(String[] args){
+        log.info("12345");
+    }
+}
---
+++ src/main/webapp/js/MyScript1.js
@@ -0,0 +1,6 @@
+/**
+ * Created by RQ on 2017/7/13.
+ */
+function myfunc() {
+    document.getElementById("demo").innerHTML="你好";
+}
